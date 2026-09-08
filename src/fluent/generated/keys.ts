2baa5e90a97f671a81fe0e77e1f43e889ae5ceed
Subashini U <subashini.u@prodapt.com>
2026-09-08 18:18:27 +0530

    16

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -266,7 +266,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '10d31c7b93c7c710f1d1f0b45d03d65f'
-                        deleted: false
+                        deleted: true
                         key: {
                             name: 'var__m_sys_hub_flow_input_852e935663994833b45cc60e4f999819'
                             element: 'table_name'
@@ -356,7 +356,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '1cd31c7b93c7c710f1d1f0b45d03d64e'
-                        deleted: false
+                        deleted: true
                         key: {
                             model: '852e935663994833b45cc60e4f999819'
                             element: 'table_name'
@@ -425,7 +425,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '30d35c7b93c7c710f1d1f0b45d03d62d'
-                        deleted: false
+                        deleted: true
                         key: {
                             model: 'f8d35c7b93c7c710f1d1f0b45d03d601'
                             element: 'changed_fields'
@@ -503,7 +503,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '34d35c7b93c7c710f1d1f0b45d03d61c'
-                        deleted: false
+                        deleted: true
                         key: {
                             name: 'var__m_sys_hub_flow_input_f8d35c7b93c7c710f1d1f0b45d03d601'
                             element: 'current'
@@ -632,7 +632,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '45d35c7b93c7c710f1d1f0b45d03d64b'
-                        deleted: false
+                        deleted: true
                         key: {
                             name: 'var__m_sys_hub_flow_input_f8d35c7b93c7c710f1d1f0b45d03d601'
                             element: 'table_name'
@@ -777,7 +777,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '54d31c7b93c7c710f1d1f0b45d03d663'
-                        deleted: false
+                        deleted: true
                         key: {
                             model: '852e935663994833b45cc60e4f999819'
                             element: 'current'
@@ -806,7 +806,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '5cd31c7b93c7c710f1d1f0b45d03d669'
-                        deleted: false
+                        deleted: true
                         key: {
                             name: 'var__m_sys_hub_flow_input_852e935663994833b45cc60e4f999819'
                             element: 'current'
@@ -1020,7 +1020,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: '70d35c7b93c7c710f1d1f0b45d03d634'
-                        deleted: false
+                        deleted: true
                         key: {
                             name: 'var__m_sys_hub_flow_input_f8d35c7b93c7c710f1d1f0b45d03d601'
                             element: 'changed_fields'
@@ -1127,7 +1127,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '7cd35c7b93c7c710f1d1f0b45d03d603'
-                        deleted: false
+                        deleted: true
                         key: {
                             model: 'f8d35c7b93c7c710f1d1f0b45d03d601'
                             element: 'current'
@@ -1178,7 +1178,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '85d35c7b93c7c710f1d1f0b45d03d638'
-                        deleted: false
+                        deleted: true
                         key: {
                             model: 'f8d35c7b93c7c710f1d1f0b45d03d601'
                             element: 'table_name'
@@ -1257,7 +1257,7 @@ declare global {
                     {
                         table: 'sys_hub_flow_input'
                         id: '90d31c7b93c7c710f1d1f0b45d03d66e'
-                        deleted: false
+                        deleted: true
                         key: {
                             model: '852e935663994833b45cc60e4f999819'
                             element: 'changed_fields'
@@ -1700,7 +1700,7 @@ declare global {
                     {
                         table: 'sys_documentation'
                         id: 'd4d31c7b93c7c710f1d1f0b45d03d675'
-                        deleted: false
+                        deleted: true
                         key: {
                             name: 'var__m_sys_hub_flow_input_852e935663994833b45cc60e4f999819'
                             element: 'changed_fields'
