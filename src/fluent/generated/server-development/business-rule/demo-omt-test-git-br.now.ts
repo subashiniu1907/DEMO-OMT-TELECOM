344736b0c8e74356affd1325c7ffd47533a8c137
Subashini U <subashini.u@prodapt.com>
2026-09-08 18:23:04 +0530

    1.1

--- src/fluent/generated/server-development/business-rule/demo-omt-test-git-br.now.ts
+++ src/fluent/generated/server-development/business-rule/demo-omt-test-git-br.now.ts
@@ -4,7 +4,7 @@ BusinessRule({
     $id: Now.ID['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'],
     name: 'Demo OMT - Test Git BR',
     table: 'x_1208752_demo_o_1_order',
-    when: 'before',
+    when: 'after',
     action: ['update'],
     filterCondition: 'stateVALCHANGES^state=completed',
     script: Now.include('./demo-omt-test-git-br.server.js'),
