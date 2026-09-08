691a6e9daeaf07486566a3dd1a8cc526cc7e42e2
Subashini U <subashini.u@prodapt.com>
2026-09-08 14:45:10 +0530

    2

---
+++ src/fluent/generated/server-development/business-rule/sys_script_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1.now.ts
@@ -0,0 +1,18 @@
+import { BusinessRule } from '@servicenow/sdk/core'
+BusinessRule({
+    // demo BR
+    $id: Now.ID['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'],
+
+    name: 'Demo OMT - Test Git BR',
+
+    table: 'x_1208752_demo_o_1_order',
+
+    when: 'before',
+
+    action: ['update'],
+
+    filterCondition: 'stateVALCHANGES^state=completed',
+
+    script: Now.include('./sys_script_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.server.js'),
+
+})
